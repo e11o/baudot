adc4ed0578e55a2b278d99bc82a0add4721dde35
Esteban Sancho <esteban.sancho@gmail.com>
2011-05-12 05:11:40 -0300

    Adding glade file

--- baudot-gui.py
+++ baudot-gui.py
@@ -1,50 +1,44 @@
-#!/usr/bin/env python
-
-import pygtk
-pygtk.require('2.0')
+import sys
 import gtk
+	
+class BaudotGUI:
 
-class Baudot:
-    
+    def on_window_destroy(self, widget, data=None):
+        gtk.main_quit()
+
+    def on_destinationCombo_changed(self, widget, data=None):
+        self.destinationChooser.set_sensitive(widget.get_active() == 1)
+     
+    def __init__(self):
+        builder = gtk.Builder()
+        builder.add_from_file("gui/baudot.glade") 
+        
+        self.window = builder.get_object("window")
+        builder.connect_signals(self)
+
+        self.destinationChooser = builder.get_object("destinationFileChooser")
+
+        # create tree for displaying files and folders
+        self.filesTreeStore = gtk.TreeStore(str, str, str, str)
+        filesTree = builder.get_object("fileView")
+        filesTree.set_model(self.filesTreeStore)
+        filesColumn = self.create_column("Files")
+        sizeColumn = self.create_column("Size")
+        descriptionColumn = self.create_column("Description")
+        charsetColumn = self.create_column("Charset")
+        filesTree.append_column(filesColumn)
+        filesTree.append_column(sizeColumn)
+        filesTree.append_column(descriptionColumn)
+        filesTree.append_column(charsetColumn)
+        
     def create_column(self, name):
         column = gtk.TreeViewColumn(name)
         cellRenderer = gtk.CellRendererText()
         column.pack_start(cellRenderer, True)
         return column
         
-    def __init__(self):
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.set_title("Baudot")
-        window.set_size_request(800, 600)
-        window.connect("delete_event", lambda w,e: gtk.main_quit())
-        
-        # create main panel
-        mainPanel = gtk.VPaned()
-        window.add(mainPanel)
-        mainPanel.show()
-
-        # create tree for displaying files and folders
-        filesTreeStore = gtk.TreeStore(str, str, str, str)
-        filesTree = gtk.TreeView(filesTreeStore)
-        self.filesColumn = self.create_column("Files")
-        self.sizeColumn = self.create_column("Size")
-        self.descriptionColumn = self.create_column("Description")
-        self.charsetColumn = self.create_column("Charset")
-        filesTree.append_column(self.filesColumn)
-        filesTree.append_column(self.sizeColumn)
-        filesTree.append_column(self.descriptionColumn)
-        filesTree.append_column(self.charsetColumn)
-
-        # add tree to main panel
-        mainPanel.add1(filesTree)
-        filesTree.show()
-
-        # finally show the window
-        window.show()
-
-    def main(self):
-        gtk.main()
         
 if __name__ == "__main__":
-    app = Baudot()
-    app.main()
+    baudot = BaudotGUI()
+    baudot.window.show()
+    gtk.main()
