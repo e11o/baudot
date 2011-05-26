64690fe1d9fcc33140edb63477f395a39793ea96
Esteban Sancho <esteban.sancho@gmail.com>
2011-05-26 01:18:36 -0300

    Applying major UI refactoring and adding UML diagrams

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
-import sys
 import os.path
 import gtk
+import gobject
 import logging
 from functools import partial
 import threading
@@ -16,72 +16,39 @@ log.setLevel(logging.INFO)
 
 TYPE_FOLDER = "Folder"
 
-class BaudotGUI(object):
+class App(object):
+    def __init__(self):
+        self.win = MainWindow()
+
+    def start(self):
+        self.win.show()
+        gtk.main()
+
+class MainWindow(object):
+
+    def __init__(self):
+        self.encoder = FileEncoder()
+        self.file_manager = FileManager(self.encoder)
+        builder = gtk.Builder()
+        builder.add_from_file("glade/window.glade")
+        builder.connect_signals(self)
+        self.win = builder.get_object("window")
+        self.dst_chooser = builder.get_object("dstFileChooser")
+
+        # TODO: review is this is the best approach
+        tree = builder.get_object("fileView")
+        tree.set_model(self.file_manager)
 
     def on_addBtn_clicked(self, widget, data=None):
-        chooser = gtk.FileChooserDialog(title="Choose file ..",
-                                        action=gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER,
-                                        buttons=(gtk.STOCK_CANCEL,
-                                                gtk.RESPONSE_CANCEL,
-                                                gtk.STOCK_OPEN,
-                                                gtk.RESPONSE_OK))
-        chooser.set_default_response(gtk.RESPONSE_OK)
-        chooser.set_select_multiple(True)
-
-        filter = gtk.FileFilter()
-        filter.set_name("Folders and text files")
-        filter.add_mime_type("text/*")
-        #chooser.add_filter(filter)
-
+        chooser = FileDirChooser()
         if chooser.run() == gtk.RESPONSE_OK:
-            files = []
-            filenames = chooser.get_filenames()
-            chooser.destroy()
-            self.in_progress = 0
-            self.completed = 0
-            for path in filenames:
-                entry = FileEntry.from_path(path)
-                self.in_progress += entry.tree_count
-                files.append(entry)
-            thread = threading.Thread(target=partial(self.process_files, files))
-            thread.start()
-            self.add_files_msg.set_label("Processing files...")
-            self.update_add_progress()
-            self.add_files_dlg.show()
-            thread.join()
-            self.files.append(files)
-
-    def update_add_progress(self):
-        log.info("Updating progress bar: completed=%d in_progress=%d" % (self.completed, self.in_progress))
-        self.add_files_progress.set_fraction(self.completed / self.in_progress)
-        if (self.completed == self.in_progress):
-            self.add_files_dlg.destroy()
-
-    def process_files(self, files):
-        for file in files:
-            self.add_tree_entry(None, file)
-
-    def add_tree_entry(self, parent, file):
-        path = file.path if parent is None else file.filename
-        mime = file.type
-        size = charset = mime = icon = None
-        if file.type == TYPE_FOLDER:
-            size = "%d items" % file.size
-            icon = "folder"
+            files = chooser.get_selection()
+            chooser.close()
+            #TODO: show "in progress" dialog
+            for file in files:
+                self.file_manager.add_file(file)
         else:
-            charset = self.encoder.detect_encoding(file.path)
-            if file.size < 1000:
-                size = "%d B" % 1000
-            elif file.size < 1000000:
-                size = "%.2f KB" % (file.size / 1000)
-            else:
-                size = "%.2f MB" % (file.size / 1000000)
-            icon = "text-x-script"
-        p = self.file_store.append(parent,(path, size, mime, charset, icon))
-        self.completed += 1
-        self.update_add_progress()
-        for child in sorted(file.children):
-            self.add_tree_entry(p, child)
+            chooser.close()
 
     def on_addMenuItem_activate(self, widget, data=None):
         self.on_addBtn_clicked(widget, data)
@@ -89,69 +56,134 @@ class BaudotGUI(object):
     def on_window_destroy(self, widget, data=None):
         gtk.main_quit()
 
-    def on_destinationCombo_changed(self, widget, data=None):
+    def on_dstCmb_changed(self, widget, data=None):
         self.dst_chooser.set_sensitive(widget.get_active() == 1)
 
-    def __init__(self):
-        self.files = []
-        self.encoder = FileEncoder()
+    def show(self):
+        self.win.show()
 
-        builder = gtk.Builder()
-        builder.add_from_file("gui/baudot.glade")
-        builder.connect_signals(self)
 
-        self.window = builder.get_object("window")
-        self.dst_chooser = builder.get_object("dstFileChooser")
-        self.file_store = builder.get_object("fileStore")
-        self.add_files_dlg = builder.get_object("addFilesDialog")
-        self.add_files_msg = builder.get_object("addFilesMessageLbl")
-        self.add_files_progress = builder.get_object("addFilesProgressBar")
+class FileManager(gtk.TreeStore):
 
-class FileEntry(object):
+    def __init__(self, encoder):
+        # path, icon, filename, size, description, charset
+        super(FileManager, self).__init__(gobject.TYPE_STRING,
+                                        gobject.TYPE_STRING,
+                                        gobject.TYPE_STRING,
+                                        gobject.TYPE_STRING,
+                                        gobject.TYPE_STRING,
+                                        gobject.TYPE_STRING)
+        self.encoder = encoder
 
-    def __init__(self, path, type, children=(), tree_count=0):
-        self.path = path
-        self.filename = os.path.basename(path)
-        self.type = type
-        self.tree_count = tree_count
-        self.children = children
-        info = os.stat(path)
-        self.size = len(children) if self.type == TYPE_FOLDER else info.st_size
+    def search(self, path):
+        path = path.strip()
+        def search(rows, path):
+            if not rows: return None
+            for row in rows:
+                if row[0] == path:
+                    return row
+                if path.startswith(row[0]):
+                    result = search(row.iterchildren(), path)
+                    if result: return result
+            return None
+        return search(self, path)
 
-    @staticmethod
-    def from_path(path=None):
-        log.debug("Creating entry for path: %s" % path)
+    def add_file(self, path, parent=None):
+        filename = path if parent is None else os.path.basename(path)
         if os.path.isdir(path):
-            children = []
-            tree_count = 0
-            for f in os.listdir(path):
-                entry = FileEntry.from_path(os.path.join(path, f))
-                if not entry is None:
-                    children.append(entry)
-                    tree_count += entry.tree_count
-            if len(children) > 0:
-                return FileEntry(path, TYPE_FOLDER, children, len(children) + tree_count)
+            it = self.append(parent, (path, "folder", filename, 0, "Folder", None))
+
+            # TODO: fix children order
+            children = os.listdir(path)
+            def folders_first(str):
+                prefix = "0" if os.path.isdir(str) else "1"
+                return prefix + str
+            for child in sorted(children, key=folders_first):
+                self.add_file(os.path.join(path, child), it)
+
+            # remove empty or set size
+            size = self.iter_n_children(it)
+            if size > 0 or parent is None:
+                self.set_value(it, 3, "%d items" % size)
+            else:
+                self.remove(it)
         else:
             ms = magic.open(magic.MAGIC_NONE)
             ms.load()
             type = ms.file(path)
             ms.close()
-            log.debug("%s is of type %s" % (path, type))
             # only allow text files
             if "text" in type.lower():
-                return FileEntry(path, type)
+                charset = self.encoder.detect_encoding(path)
+                info = os.stat(path)
+                if info.st_size < 1000:
+                    size = "%d B" % info.st_size
+                elif info.st_size < 1000000:
+                    size = "%.2f KB" % (info.st_size / 1000)
+                else:
+                    size = "%.2f MB" % (info.st_size / 1000000)
+                self.append(parent, (path, "text-x-script", filename, size, type, charset))
 
-    def __cmp__(self, other):
-        if self.type == TYPE_FOLDER:
-            if other.type == TYPE_FOLDER:
-                return cmp(self.filename, other.filename)
-            return -1
-        else:
-            if other.type == TYPE_FOLDER:
-                return 1
-            return cmp(self.filename, other.filename)
+
+class FileDirChooser(object):
+
+    def __init__(self):
+        self.selection = None
+        #TODO: create file or folder single dialog
+        self.dialog = gtk.FileChooserDialog(title="Choose file ..",
+                                        action=gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER,
+                                        buttons=(gtk.STOCK_CANCEL,
+                                                gtk.RESPONSE_CANCEL,
+                                                gtk.STOCK_OPEN,
+                                                gtk.RESPONSE_OK))
+        self.dialog.set_default_response(gtk.RESPONSE_OK)
+        self.dialog.set_select_multiple(True)
+
+        #filter = gtk.FileFilter()
+        #filter.set_name("Folders and text files")
+        #filter.add_mime_type("text/*")
+        #chooser.add_filter(filter)
+
+    def run(self):
+        response = self.dialog.run()
+        if  response == gtk.RESPONSE_OK:
+            self.selection = self.dialog.get_filenames()
+        return response
+
+    def get_selection(self):
+        return self.selection
+
+    def close(self):
+        self.dialog.destroy()
+
+
+class InProgressDialog(object):
+
+    def run(self):
+        pass
+
+    def set_message(self, message):
+        pass
+
+    def update_progress(self, progress):
+        pass
+
+    def close(self):
+        pass
+
+
+class EncodingChooser(object):
+
+    def run(self):
+        pass
+
+    def get_selected_encoding(self):
+        pass
+
+    def close(self):
+        pass
+
 
 if __name__ == "__main__":
-    baudot = BaudotGUI()
-    baudot.window.show()
-    gtk.main()
+    app = App()
+    app.start()
